f246b4e300c773e9f6e914e1481dc475d20c1fb2
supercoolperson <ericdu.awesome@gmail.com>
2024-04-17 09:06:39 -0700

    jit
    jit

--- src/main.cpp
+++ src/main.cpp
@@ -12,14 +12,45 @@ int main() {
 	camera.transform.position = {0, 0, 1};
 
 	solid cube(&camera);
-	cube.add_vertex({-0.5, -0.5, -0.5});
-	cube.add_vertex({-0.5, -0.5, 0.5});
-	cube.add_vertex({-0.5, 0.5, -0.5});
-	cube.add_vertex({-0.5, 0.5, 0.5});
-	cube.add_vertex({0.5, -0.5, -0.5});
-	cube.add_vertex({0.5, -0.5, 0.5});
-	cube.add_vertex({0.5, 0.5, -0.5});
-	cube.add_vertex({0.5, 0.5, 0.5});
+	solid icosahedron(&camera); 
+	//cube.add_vertex({-0.5, -0.5, -0.5});
+	//cube.add_vertex({-0.5, -0.5, 0.5});
+	//cube.add_vertex({-0.5, 0.5, -0.5});
+	//cube.add_vertex({-0.5, 0.5, 0.5});
+	//cube.add_vertex({0.5, -0.5, -0.5});
+	//cube.add_vertex({0.5, -0.5, 0.5});
+	//cube.add_vertex({0.5, 0.5, -0.5});
+	//cube.add_vertex({0.5, 0.5, 0.5});
+	//cube.add_vertex({0.5, 0.5, 0.5});
+
+	float jit = 5 / (sqrt(50 - (10 * sqrt(5))));
+
+	cube.add_vertex({0, 0, -jit}); 
+	cube.add_vertex({0, 0, jit}); 
+
+	float jits = sqrt(2 / (5 - sqrt(5)));
+	jit = 1 / (sqrt(10 - (2 * sqrt(5))));
+
+	cube.add_vertex({-jits, 0, -jit}); 
+	cube.add_vertex({jits, 0, jit}); 
+
+	jits = (1 + sqrt(5))/(2 * sqrt(10 - (2 * sqrt(5))));
+
+	cube.add_vertex({jits, -0.5, -jit}); 
+	cube.add_vertex({jits, 0.5, -jit}); 
+	cube.add_vertex({-jits, -0.5, jit}); 
+	cube.add_vertex({-jits, 0.5, jit}); 
+
+	jits = (-1 + sqrt(5)) / (2 * sqrt(10 - (2 * sqrt(5))));
+	float jitses = 0.5 * sqrt((5 + sqrt(5))/(5 - sqrt(5))); 
+
+	cube.add_vertex({-jits, -jitses, -jit});
+	cube.add_vertex({-jits, jitses, -jit});
+	cube.add_vertex({jits, -jitses, jit});
+	cube.add_vertex({jits, jitses, jit});
+
+
+
 	cube.transform.position = {500, 500, 0};
 	cube.transform.scale = {100, 100, 100};
 
