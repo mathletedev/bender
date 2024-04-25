94bc20c358f35611051395f80bf7c7c82280ded7
CharlesLiuCool <148991486+CharlesLiuCool@users.noreply.github.com>
2024-04-24 21:45:56 -0700

    Update test.cpp

--- src/test.cpp
+++ src/test.cpp
@@ -1,25 +1,60 @@
 #include "test.hpp"
 
-void test::test_transform() {
+void test::test_x_transform() {
 	transform testData;
+	sf::Vector3f vertex(0,0,0);
 	testData.position = sf::Vector3f(1,0,0);
-	matrix testMatrix = testData.to_matrix() * sf::Vector3f(0,0,0);
+	matrix testMatrix = testData.to_matrix() * vertex;
 	if (testMatrix.get(0,0) == 1) {
-		std::cout << "Test transform passed!" << std::endl;
+		std::cout << "Test x transform passed!" << std::endl;
 	}
 	else {
-		std::cout << "Test transform failed!" << std::endl;
+		std::cout << "Test x transform failed!" << std::endl;
 	}
 }
 
-void test::test_rotation() {
+void test::test_y_transform() {
+	transform testData;
+	sf::Vector3f vertex(0,0,0);
+	testData.position = sf::Vector3f(0,1,0);
+	matrix testMatrix = testData.to_matrix() * vertex;
+	if (testMatrix.get(1,0) == 1) {
+		std::cout << "Test y transform passed!" << std::endl;
+	}
+	else {
+		std::cout << "Test y transform failed!" << std::endl;
+	}
 }
 
-void test::test_scale() {
+void test::test_z_transform() {
+	transform testData;
+	sf::Vector3f vertex(0,0,0);
+	testData.position = sf::Vector3f(0,0,1);
+	matrix testMatrix = testData.to_matrix() * vertex;
+	if (testMatrix.get(2,0) == 1) {
+		std::cout << "Test z transform passed!" << std::endl;
+	}
+	else {
+		std::cout << "Test z transform failed!" << std::endl;
+	}
 }
+
 
 void test::test_transform_on_empty() {
 }
 
-void test::test_import_obj() {
+void test::test_rotation() {
+	transform testData;
+	sf::Vector3f vertex(1,0,0);
+	testData.rotation = sf::Vector3f(1,0,0);
+	matrix testMatrix = testData.to_matrix() * vertex;
+	if (testMatrix.get(0,0) == 1) {
+		std::cout << "Test rotation passed!" << std::endl;
+	}
+	else {
+		std::cout << "Test rotation failed!" << std::endl;
+	}
+}
+
+void test::test_scale() {
 }
