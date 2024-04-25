7fa4f2831c518c4d6f57631626ac688c39ea3ed5
CharlesLiuCool <148991486+CharlesLiuCool@users.noreply.github.com>
2024-04-24 21:33:10 -0700

    Update test.cpp

--- src/test.cpp
+++ src/test.cpp
@@ -1,5 +1,17 @@
 #include "test.hpp"
 
-void test::test_rotation() {
+void test::test_transform() {
 	Transform testData;
+}
+
+void test::test_rotation() {
+}
+
+void test::test_scale() {
+}
+
+void test::test_transform_on_empty() {
+}
+
+void test::test_import_obj() {
 }
